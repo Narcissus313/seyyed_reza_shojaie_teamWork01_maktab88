663e9008985cfe2deba401bef1537830b7aab0df
narcissus313 <srz.shl@gmail.com>
2023-01-22 15:30:19 +0330

    front ended

--- script/main.js
+++ script/main.js
@@ -1,66 +1,41 @@
 $(() => {
+	let data = [];
+	let countriesNames = [];
+
+	function fillCountriesNames() {
+		for (const country of countryDatas) {
+			countriesNames.push(country["name"]["common"]);
+		}
+		countriesNames.sort((a, b) => a.localeCompare(b));
+	}
+
+	function setFormSelectMenu() {
+		for (const country of countriesNames) {
+			if (country !== "Iran") {
+				$("#formSelectCountries").append(
+					`<option value="${country}">${country}</option>`
+				);
+			} else {
+				$("#formSelectCountries").append(
+					`<option class="text-danger fs-5 fst-italic" value="${country}"><a class="text-Success">${country}</a></option>`
+				);
+			}
+		}
+	}
 
 	$.ajax({
-		url: "https://reqres.in/api/users",
-		type: "POST",
-		data: {
-		},
+		url: "https://restcountries.com/v3.1/all",
+		type: "GET",
 		success: function (response) {
-			console.log(response);
+			console.log(response[0]);
+			countryDatas = response;
 		},
 		error: function (err) {
 			console.log(err);
 		},
-	},
-		{
-			url: 'https://reqres.in/api/users/3',
-			type: 'GET',
-			success: function (response) {
-				console.log(response);
-			},
-			error: function (err) {
-				console.log(err);
-			}
-		}
-);
+		async: false,
+	});
 
-
-	// $.ajax({
-	// 	url: 'https://reqres.in/api/users/3',
-	// 	type: 'GET',
-	// 	success:function (response) {
-	// 		console.log(response);
-	// 	},
-	// 	error:function (err) {
-	// 		console.log(err);
-	// 	},
-	// });
-
-
-
-
-	// address: https://reqres.in/api/users/3
-// method: GET
-// response body:
-// {
-//     "data": {
-//         "id": 3,
-//         "email": "janet.weaver@reqres.in",
-//         "first_name": "Janet",
-//         "last_name": "Weaver",
-//         "avatar": "https://reqres.in/img/faces/2-image.jpg"
-//     }
-// }
-// address: https://reqres.in/api/users/3
-// method: GET
-// response body:
-// {
-//     "data": {
-//         "id": 3,
-//         "email": "janet.weaver@reqres.in",
-//         "first_name": "Janet",
-//         "last_name": "Weaver",
-//         "avatar": "https://reqres.in/img/faces/2-image.jpg"
-//     }
-// }
+	fillCountriesNames();
+	setFormSelectMenu();
 });

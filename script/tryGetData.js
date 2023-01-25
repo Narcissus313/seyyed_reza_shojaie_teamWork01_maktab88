1e52c6ae01d3cfec9f2228212095a502004f5f2a
nimabht <nimabeheshtaein99@gmail.com>
2023-01-25 18:48:59 +0330

    try get data fixed.

--- script/tryGetData.js
+++ script/tryGetData.js
@@ -1,7 +1,9 @@
 function tryGetData(targetCountry, key, index = null) {
   try {
-    if (index === null) return targetCountry[key];
-    else return targetCountry[key][index];
+    if (index === null) {
+      if (targetCountry[key]) return targetCountry[key];
+      else return "no Info";
+    } else return targetCountry[key][index];
   } catch (error) {
     return "No Info";
   }
